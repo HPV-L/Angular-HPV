e34513124899354c9ad2cfeab6f5a69c2c47d78f
hoangbru <hoangbru07@gmail.com>
2023-05-28 23:18:18 +0700

    edit folder name layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { BaseLayoutComponent } from './Layout/base-layout/base-layout.component';
+import { BaseLayoutComponent } from './layout/base-layout/base-layout.component';
 import { BannerComponent } from './component/banner/banner.component';
 
 const routes: Routes = [
