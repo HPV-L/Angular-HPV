2c319bf2941fb23b3c8bcbd8d863e1ad3d68b52e
Ptran <106719083+Migu03@users.noreply.github.com>
2023-05-28 22:50:35 +0700

    feature: Ul homepage refs:AH-4

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BaseLayoutComponent } from './Layout/base-layout/base-layout.component';
+import { BannerComponent } from './component/banner/banner.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", component: BaseLayoutComponent, children:[
+    {path:"",component:BannerComponent},
+  ]}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
